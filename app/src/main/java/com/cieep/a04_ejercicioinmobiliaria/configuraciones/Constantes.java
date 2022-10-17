b49b7c478cf4f332893167ef5d0d12dc20cb524c
eduard <eduard@cieep.com>
2022-10-17 17:58:49 +0200

    Initial commit

---
+++ app/src/main/java/com/cieep/a04_ejercicioinmobiliaria/configuraciones/Constantes.java
@@ -0,0 +1,5 @@
+package com.cieep.a04_ejercicioinmobiliaria.configuraciones;
+
+public class Constantes {
+    public static final String INMUEBLE = "inmueble";
+}
---
+++ app/src/main/java/com/cieep/a04_ejercicioinmobiliaria/modelos/Inmueble.java
@@ -0,0 +1,82 @@
+package com.cieep.a04_ejercicioinmobiliaria.modelos;
+
+import java.io.Serializable;
+
+public class Inmueble implements Serializable {
+
+    private String direccion;
+    private int numero;
+    private String cp;
+    private String ciudad;
+    private String provincia;
+    private float valoracion;
+
+    public float getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(float valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public Inmueble(String direccion, int numero, String cp, String ciudad, String provincia, float valoracion) {
+        this.direccion = direccion;
+        this.numero = numero;
+        this.cp = cp;
+        this.ciudad = ciudad;
+        this.provincia = provincia;
+        this.valoracion = valoracion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    @Override
+    public String toString() {
+        return "Inmueble{" +
+                "direccion='" + direccion + '\'' +
+                ", numero=" + numero +
+                ", cp='" + cp + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                ", valoracion=" + valoracion +
+                '}';
+    }
+}
